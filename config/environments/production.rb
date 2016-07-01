aa3d00132337699ae1bb2bc3f1339f4a12741fa8
Sera <sera@netlab.jp>
2016-07-01 12:03:05 +0900

    アセットプリコンパイル先を指定(refs #3629)

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -35,7 +35,7 @@ Rails.application.configure do
   # yet still be able to expire them through the digest params.
   config.assets.digest = true
 
-  config.assets.prefix = '/assets/gem_watcher'
+  config.assets.prefix = '/gem_watcher/assets'
 
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
