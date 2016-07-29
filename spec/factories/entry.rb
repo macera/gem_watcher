43ba21b685865f04ccdaea0baffd074f905247e4
Sera <sera@netlab.jp>
2016-07-29 13:46:05 +0900

    仕様変更によるfactoryの修正(refs #3662)

--- spec/factories/entry.rb
+++ spec/factories/entry.rb
@@ -1,21 +1,29 @@
 FactoryGirl.define do
   factory :entry do
-    title        "v5.0.0.rc1"
+    title        "rails (5.0.0)"
     published    { Time.now }
-    content      "<p>v5.0.0 release</p>"
-    url          "http://github.com/plugin/plugin/releases/tag/v5.0.0"
+    content      "Ruby on Rails is a full-stack web framework"
+    url          "https://rubygems.org/gems/rails/versions/5.0.0"
     author       "author name"
     plugin       { create(:plugin, name: 'rails') }
+    major_version 5
+    minor_version 0
+    patch_version 0
+  end
+
+  factory :rails_entry, class: Entry do
+
   end
 
   factory :nokogiri_entry, class: Entry do
     title        "1.6.8"
     published    { Time.now }
-    content      "Nokogiri (鋸) is an HTML, XML, SAX, and Reader parser.  Among
-Nokogiri&amp;#39;s many features is the ability to search documents via XPath
-or CSS3 selectors."
+    content      "Nokogiri (鋸) is an HTML, XML, SAX, and Reader parser."
     url          "https://rubygems.org/gems/nokogiri/versions/1.6.8"
     author       "author name"
     plugin       { create(:plugin, name: 'nokogiri') }
+    major_version 1
+    minor_version 6
+    patch_version 8
   end
 end
