0063f8d88d6cc0f231b791fb967097edb4ce060c
Sera <sera@netlab.jp>
2016-08-15 16:39:55 +0900

    不要なファイルの削除(refs #3628)

--- app/controllers/plugins_controller.rb
+++ app/controllers/plugins_controller.rb
@@ -1,17 +1,10 @@
 class PluginsController < ApplicationController
-  include GemWatcher::ScrapeCveNumber
-
   before_action :set_plugin, only: [:show]
 
   # gem詳細画面
   def show
     @release_feeds = @plugin.entries.order('published desc').limit(5)
     @securities = @plugin.security_entries
-    # source_code_uriがない場合もあるためひとまずこれでしのぐ
-    if @plugin.source_code_uri
-      uri = URI.join(@plugin.source_code_uri, 'blob/master/CHANGELOG.md')
-      @cve_numbers = cve_numbers(uri.to_s)
-    end
   end
 
   private
